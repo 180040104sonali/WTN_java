e0907eda04456897d5e5c92461ffaab2e00c2749
sonali <sonali@192.168.0.8>
2021-05-24 10:10:15 +0530

    strings

---
+++ string/src/string/String6.java
@@ -0,0 +1,18 @@
+package string;
+
+public class String6 {
+
+	public static void main(String[] args) {
+		String a = "hi";
+		String b = "hello";
+		String output;
+
+		if (a.length() < b.length()) 
+			output = a + b + a;
+		else
+			output = b + a + b;
+		
+		System.out.println(output);
+	}
+
+}
